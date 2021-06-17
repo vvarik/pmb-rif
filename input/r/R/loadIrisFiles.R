3c5d78fde31bd810df084d80398a21b43baffcd7
vvarik <vvarik.mail@gmail.com>
2021-06-17 17:07:11 +0200

    Add function to load iris files

---
+++ input/r/R/loadIrisFiles.R
@@ -0,0 +1,32 @@
+#' Load iris files 
+#'
+#' Load all iris files
+#' @param folder.path
+#' @param keep.folder.name
+#' @export
+loadIrisFiles = function(folder.path, keep.folder.name = F){
+
+  loadOneIrisFile = function(iris.filename){
+    iris.file.data = data.table::fread(iris.filename)
+    
+    iris.file.data[,filename:=iris.filename]
+    
+    return(iris.file.data)
+  }
+  
+  all.iris.filenames = paste(folder.path, 
+    list.files(path = folder.path, pattern = "\\.iris$", all.files = T),
+    sep="/")
+  
+  all.iris.files.data =
+    data.table::rbindlist(
+      lapply(all.iris.filenames, loadOneIrisFile)
+    )
+  
+  
+  if(!keep.folder.name){
+    all.iris.files.data[,filename:=basename(filename)]
+  }
+  
+  return(all.iris.files.data)
+}
