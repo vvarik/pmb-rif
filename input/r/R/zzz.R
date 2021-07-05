f179418eec6623cf8c972b8c4c4d0583514ebb35
vvarik <vvarik.mail@gmail.com>
2021-07-05 16:30:29 +0200

    Add drugCols variable

--- input/r/R/zzz.R
+++ input/r/R/zzz.R
@@ -20,6 +20,13 @@
   # The palette with black:
   ass('cbbPalette', c("#000000", "#E69F00", "#56B4E9", "#CC79A7", "#009E73",
   "#D55E00", "#F0E442", "#0072B2"))
+
+  ass('drugCols', 
+    c(RifMono = '#a40000', PmbMono = '#e69e00', Cmb = '#000000',
+      RifWt   = '#ef2929', PmbWt = '#fcaf3e', CmbWt = '#888a85',
+      LoeweNullWt = '#babdb6', LoeweNullMut = '#2e3436'
+     )
+  )
   
   ass('myBreaks', c(0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 1.28))
   
