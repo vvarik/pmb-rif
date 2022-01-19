6dcb9cd45ae8456aee3e09bcad20a6e28e58a26e
Vallo Varik <varik.mail@gmail.com>
2022-01-19 22:40:15 +0100

    Add function to load DDI data

--- input/r/R/util.R
+++ input/r/R/util.R
@@ -1101,3 +1101,22 @@ getCs = function(fit) {
     mutate(par = ifelse(grepl(1, par), 'cs1', 'cs2')) %>% 
     arrange(cond, par)
 }
+
+
+#' @export
+loadDdiData = function(fname) {
+  readRDS(fname) %>% 
+  as.data.table() %>% 
+  .[, effect := round(effect, 3)] %>% 
+
+  # incomplete data interferes with surface analysis methods
+  .[!(cond=='intra' & strain=='ATCC27853' &
+    !(c1 %in% c(0, 0.0016, 0.016, 0.048, 0.16, 0.48, 
+     4.8, 16, 48, 160, 480, 1600) &
+    c2 %in% c(0, 0.0004, 0.003, 0.004, 0.012, 0.03, 
+      0.12, 0.3, 1, 1.2, 3, 4, 30, 40, 60, 200)))] %>% 
+  # arrive at informative and uniform concentration range
+  .[(d1 %between% c(0.003, 100) | d1==0) & 
+    (d2 %between% c(0.003, 100) | d2==0)] %>% 
+  .[is.na(effect), effect := -4.5]
+}
